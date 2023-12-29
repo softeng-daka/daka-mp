2500ac09337f4ebcb1990cbfb983ee32533e21ae
marcus1181 <143788057+marcus1181@users.noreply.github.com>
2023-12-29 20:27:52 +0800

    Add files via upload

--- src/pages/coffeinfo/index.vue
+++ src/pages/coffeinfo/index.vue
@@ -8,7 +8,7 @@
 
     <div class="rounded-rectangle">
       <p>商品信息</p>
-      <button @click="addFavorite">点击添加收藏</button>
+      <button class="keyi" @click="addFavorite">添加收藏</button><button class="keyi2" @click="removeFavorite">取消收藏</button>
     </div>
 
     <div>
@@ -163,6 +163,42 @@ export default {
       console.error(err);
     });
     };
+    const removeFavorite=() =>{
+    const users = db.collection('users');
+    const gidx=gid.value;
+      const opidx=opid.value;
+
+    // 首先检查用户的 like 数组中是否包含要删除的商品 id
+    users.where({
+      _openid: opidx,
+      like: _.in([gidx])
+    }).get().then(res => {
+      if (res.data.length > 0) {
+        // 用户的 like 数组中包含要删除的商品 id
+        // 在数组中找到该商品 id 并移除
+        const updatedLike = res.data[0].like.filter(id => id !== gidx);
+
+        // 更新用户的 like 数组
+        users.where({
+          _openid:opidx
+        })
+          .update({
+            data: {
+              like: updatedLike
+            }
+          }).then(updateRes => {
+          console.log('商品已从喜爱列表中移除');
+        }).catch(updateErr => {
+          console.error(updateErr);
+        });
+      } else {
+        // 用户的 like 数组中不包含要删除的商品 id
+        console.log('该商品不在喜爱列表中');
+      }
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 
     onMounted(async () => {
       const params = Taro.getCurrentInstance().router?.params;
@@ -213,12 +249,20 @@ export default {
       sendComment,
       reloadComment,
       gid,
-      comments
+      comments,
+      removeFavorite,
     };
   },
 };
 </script>
 <style>
+.keyi{
+  margin-right: 0px;
+
+}
+.keyi2{
+  margin-left:0px;
+}
 .title{
   font-size:   60px;
   color: #F0202A;
